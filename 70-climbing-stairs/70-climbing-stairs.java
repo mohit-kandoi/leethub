d914b87b4da3a6d0b2ad10aad132a9105f97b7e7
mohit-kandoi <64693304+mohit-kandoi@users.noreply.github.com>
2022-02-07 14:57:03 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.17%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        return (int)((Math.pow(((1+Math.sqrt(5))/2),n+1)-Math.pow(((1-Math.sqrt(5))/2),n+1))/Math.sqrt(5));
+        
+        
+        // int ans = (int)(((Math.pow( ((1+Math.sqrt(5)) / 2 ), n+1)) - (Math.pow( ((1-Math.sqrt(5)) / 2 ), n+1)))/ Math.sqrt(5));
+        // return ans;
+    }
+}
