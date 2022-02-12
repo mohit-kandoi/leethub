b0f5f662c2acc137bb698e62b0ab6d2f1ada93b5
mohit-kandoi <64693304+mohit-kandoi@users.noreply.github.com>
2022-02-12 23:35:23 +0530

    Time: 18 ms (63.70%), Space: 47.9 MB (5.85%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -9,5 +9,10 @@ class Solution {
         
         return n==1;
         
+            // 3^19 =1162261467, 
+	        //  3^20 is exceding  Integer_Range So 3^19 is the highest power in Integer Range  
+            // return ( n>0 &&  1162261467%n==0);
+
+        
     }
 }
