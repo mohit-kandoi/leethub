0aa91ea2aa7b34f6af91a1756a979ede1ae08a29
mohit-kandoi <64693304+mohit-kandoi@users.noreply.github.com>
2022-02-22 18:18:12 +0530

    Time: 0 ms (100.00%), Space: 43 MB (5.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,28 +1,54 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        int zeroes = 0;
-        int ones = 0;
-        int twos = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 0) zeroes++;
-            else if(nums[i] == 1) ones++;
-            else twos++;
-        }
+        //Optimized solution: O(N)
         
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
         for(int i=0; i<nums.length; i++){
-            if(zeroes != 0){
-                nums[i]=0;
-                zeroes--;
-            } 
-            else if(ones != 0){
-                nums[i] = 1;
-                ones--;
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
             }
-            else{
-                nums[i] = 2;
+            else if(nums[mid] == 2){
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }else{
+                mid++;
             }
         }
+          
+        
+       //Solution with O(2N); 
+        
+//         int zeroes = 0;
+//         int ones = 0;
+//         int twos = 0;
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i] == 0) zeroes++;
+//             else if(nums[i] == 1) ones++;
+//             else twos++;
+//         }
+        
+//         for(int i=0; i<nums.length; i++){
+//             if(zeroes != 0){
+//                 nums[i]=0;
+//                 zeroes--;
+//             } 
+//             else if(ones != 0){
+//                 nums[i] = 1;
+//                 ones--;
+//             }
+//             else{
+//                 nums[i] = 2;
+//             }
+//         }
         
     }
 }
